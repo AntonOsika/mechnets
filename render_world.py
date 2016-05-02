ca7bd848738af3beaea4a4a910221f46fdaa59ac
Anton O <anton.osika@gmail.com>
2016-05-02 20:20:34 +0200

    Antons, without collision

--- render_world.py
+++ render_world.py
@@ -1,14 +1,15 @@
 import pygame
 import sys
+import numpy as np
 from pygame.locals import *
 
 
+rgb_stainless = (224, 223, 219)
 def draw_boarder(screen, tup_game_size):
 
     x_max = tup_game_size[0]
     y_max = tup_game_size[1]
     width = 10
-    rgb_stainless = (224, 223, 219)
 
     pygame.draw.line(screen, rgb_stainless, [0, 0], [x_max, 0], width)
     pygame.draw.line(screen, rgb_stainless, [x_max, 0], [x_max, y_max], width)
@@ -29,15 +30,21 @@ screen = pygame.display.set_mode(tup_game_size)
 # Draw game boarder
 draw_boarder(screen, tup_game_size)
 color = (192,192,192)
-pos = (40, 30)
 radius = 10
-pygame.draw.circle(screen, color, pos, radius)
 
+pos = np.array((40.0, 30.0))
+v = np.array((0.1, 0.1))
+
+dt = 10
 while True:
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
+    
+    pygame.draw.circle(screen, (0,0,0), pos.astype('int'), radius)
+    pos += dt*v
+    pygame.draw.circle(screen, color, pos.astype('int'), radius)
     pygame.display.update()
+    pygame.time.wait(dt)

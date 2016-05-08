530adfe7fa54c9f239870fc4b7b6082b9adb0e38
Anton O <anton.osika@gmail.com>
2016-05-08 10:49:58 +0200

    fixed some things not finished

--- cartpole1.py
+++ cartpole1.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import numpy as np
 
 state_dim = 4
-action_dim = 1
+#action_dim = 1
 out_dim = 2
 
 N1 = 6
@@ -11,28 +12,28 @@ batchsize = 128
 graph = tf.Graph()
 with graph.as_default():
     startstate = tf.placeholder(tf.float32, shape=(batchsize, state_dim))
-    actiontaken = tf.placeholder(tf.float32, shape=(batchsize, action_dim))
-    Qtrue  = tf.placeholder(tf.int32, shape=(batchsize, 1))   # simple Selector for which Q to use
+    actiontaken = tf.placeholder(tf.bool, shape=(batchsize))
+    Qtrue  = tf.placeholder(tf.float32, shape=(batchsize))   # simple Selector for which Q to use
 
     weights1 = tf.Variable( tf.truncated_normal([state_dim, N1]))
     biases1 = tf.Variable(tf.zeros([N1]))
 
-    # Only use the in_dim state variables 
+    # Only use the state_dim state variables 
     h1 = tf.nn.softmax(tf.matmul(startstate, weights1) + biases1)
 
     weights2 = tf.Variable( tf.random_normal([N1, out_dim]))
     biases2 = tf.Variable(tf.zeros([out_dim]))
 
     Qvals = tf.matmul(h1, weights2) + biases2
-    Qpred = Qvals[:, actiontaken]]
+    Qpred = tf.select(actiontaken, Qvals[:, 0], Qvals[:, 1])
 
     loss = tf.nn.l2_loss(Qpred-Qtrue)
     optimizer = tf.train.GradientDescentOptimizer(rate).minimize(loss)
 
     Qbest = tf.maximum(Qvals[:, 0], Qvals[:, 1])
 
-    singlestate = tf.placeholder(tf.float32, shape(1,in_dim))
-    action = tf.argmax( tf.matmul( tf.matmul(singlestate, w1) + biases1, w2) + biases2))
+    singlestate = tf.placeholder(tf.float32, shape=(1,state_dim))
+    action = tf.argmax( tf.matmul( tf.matmul(singlestate, weights1) + biases1, weights2) + biases2, 1)
 
 import gym
 
@@ -42,10 +43,11 @@ steps = 801
 
 batches = 8
 memsize = batchsize*batches
-states1 = np.zeros((memsize, in_dim +1))     #With action taken
-states2 = np.zeros((memsize, in_dim + 1))    #With reward
+states1 = np.zeros((memsize, state_dim ))     #With action taken
+states1 = np.zeros((memsize), dtype='bool_')     #With action taken
+states2 = np.zeros((memsize, state_dim + 1))    #With reward
 pos = 0
-done = false
+done = False
 state = env.reset()
 
 with tf.Session(graph=graph) as session:
@@ -60,21 +62,21 @@ with tf.Session(graph=graph) as session:
                 else:
                     action = session.run(action, feed_dict={ singlestate:state })
 
-                states1[pos +i*batchsize, :in_dim] = state
-                states1[pos +i*batchsize, in_dim] = action
+                states1[pos +i*batchsize, :] = state
+                actions[pos +i*batchsize] = action
 
                 observation, reward, done, info = env.step(action)
 
                 if done:
                     reward += -10
 
-                states2[pos +i*batchsize, :in_dim] = state
-                states2[pos +i*batchsize, in_dim] = reward
+                states2[pos +i*batchsize, :state_dim] = state
+                states2[pos +i*batchsize, state_dim] = reward
         for batch in range(batches):
             # Future max Q + reward:
             offset = batch*batchsize
             Qtrue = session.run(Qbest, feed_dict={ startstate:states2[offset:(offset+batchsize),:state_dim] }) + states2[offset:(offset+batchsize), state_dim]
-            _, l = session.run([optimizer, loss], feed_dict={startstate:states1[offset:(offset+batchsize), :state_dim], actiontaken:states1[offset:(offset+batchsize), -1], Qtrue:Qtrue}
+            _, l = session.run([optimizer, loss], feed_dict={startstate:states1[offset:(offset+batchsize), :state_dim], actiontaken:states1[offset:(offset+batchsize), -1], Qtrue:Qtrue})
             if step % 100 == 0:
                 print("Loss at step", step, ":", l)
                 print("Avg Q:", Qtrue.mean())
